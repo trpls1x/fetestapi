dc6d94f7ab30b09c18be246cae9da26abd1f59f4
trpls1x <sasha.boyko537@gmail.com>
2021-10-26 17:23:01 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <cinema-header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CinemaHeader from '@/components/Header'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CinemaHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app 
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale  
+  background: #222B31
+  min-height: 100vh
+    
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-row class="header">
+        <v-col class="logo col-2 d-flex justify-center align-center">
+            <div class="d-block">FETESTAPI</div>
+        </v-col>
+        <v-col class="col-10">
+
+        </v-col>
+    </v-row>
+</template>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap')
+
+.header
+    height: 60px
+    color: #FFF
+    background: #F8424F
+    font-family: 'Russo One', sans-serif
+
+.logo
+    background: #F73745
+</style>
